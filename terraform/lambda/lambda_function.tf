aea3991b78387cfed602db13db73772bf33e0243
Erastus Thambo <erastus.thambo@refinitiv.com>
2020-06-26 14:33:05 -0500

    4th commit

--- terraform/lambda/lambda_function.tf
+++ terraform/lambda/lambda_function.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_function" "lambda_function" {
   function_name = local.name
-  role          = "arn:aws:iam::191141428331:role/lambda_role"
+  role          = "arn:aws:iam::xxxxxxxx:role/lambda_role"    #lambda role with appropriate permissions
   filename      = "../function/lambda.zip"
   handler       = var.handler
   runtime       = var.runtime
